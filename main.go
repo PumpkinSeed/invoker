28e7d3dd4e91712581cdc8e1deb94b1acc65159f
PumpkinSeed <qwer.kocka@gmail.com>
2019-11-25 10:57:24 +0100

    Finish the solution

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -14,6 +13,14 @@ import (
 	"github.com/docker/docker/client"
 )
 
+const (
+	EnvPrefix = "INVOKER_"
+
+	EnvVerbose = EnvPrefix+"VERBOSE"
+	EnvSkipOutput = EnvPrefix+"SKIP_OUTPUT"
+	EnvSettings = EnvPrefix+"SETTINGS"
+)
+
 type settings struct {
 	Containers map[string][]string `json:"containers"`
 	Commands   map[string][]string `json:"commands"`
@@ -21,13 +28,16 @@ type settings struct {
 
 func main() {
 	containerArg, commandArg := parseArgs()
+	verboseLog(fmt.Sprintf("invoker starts to execute {%s} command group on {%s} container group", commandArg, containerArg), Info)
+
 	ctx := context.Background()
-	cli, err := client.NewClient(client.DefaultDockerHost, client.DefaultVersion, nil, nil)
+	cli, err := client.NewEnvClient()
 	if err != nil {
-		panic(err)
+		verboseLog(fmt.Sprintf("%s [%s]", "error: creating Docker client connection", err.Error()), Fatal)
+		return
 	}
 
-	s, err := read()
+	s := read()
 
 	containers, err := getContainerIDs(context.Background(), cli, s.Containers[containerArg])
 	if err != nil {
@@ -36,31 +46,41 @@ func main() {
 	for _, container := range containers {
 		if commands, ok := s.Commands[commandArg]; ok {
 			for _, command := range commands {
-				data, err := exec(ctx, cli, container, command)
-				if err != nil {
-					panic(err)
-				}
-				fmt.Println(string(data))
+				data := exec(ctx, cli, container.id, command)
+				printOutput(container, command, string(data))
 			}
 		}
 	}
-	fmt.Println(containers)
 }
 
-func read() (*settings, error) {
-	path := os.Getenv("INVOKER_SETTINGS")
-	log.Print(path)
+func printOutput(container containerDef, command, data string) {
+	if skip, ok := os.LookupEnv(EnvSkipOutput); ok && skip == "true" {
+		return
+	}
+	fmt.Printf("%s [%s] ~ %s\n", removePrefix(container.name), container.id, command)
+	fmt.Print(data)
+}
+
+func read() *settings {
+	path := os.Getenv(EnvSettings)
 	if path == "" {
-		return nil, errors.New("empty")
+		verboseLog(fmt.Sprintf("%s", "error: settings path is empty, set INVOKER_SETTINGS with the path of settings file"), Fatal)
+		return nil
 	}
 
 	content, err := ioutil.ReadFile(path)
 	if err != nil {
-		return nil, err
+		verboseLog(fmt.Sprintf("%s %s [%s]", "error: reading the file on path", path, err.Error()), Fatal)
+		return nil
 	}
+
 	var s settings
 	err = json.Unmarshal(content, &s)
-	return &s, err
+	if err != nil {
+		verboseLog(fmt.Sprintf("%s [%s]", "error: marshal the file content to settings", err.Error()), Fatal)
+		return nil
+	}
+	return &s
 }
 
 func parseArgs() (string, string) {
@@ -70,43 +90,55 @@ func parseArgs() (string, string) {
 	return os.Args[1], os.Args[2]
 }
 
-func exec(ctx context.Context, cli *client.Client, container string, command string) ([]byte, error) {
+func exec(ctx context.Context, cli *client.Client, container string, command string) []byte {
 	exec, err := cli.ContainerExecCreate(ctx, container, types.ExecConfig{
 		AttachStderr: true,
 		AttachStdout: true,
 		Cmd:          strings.Split(command, " "),
 	})
 	if err != nil {
-		return nil, err
+		verboseLog(fmt.Sprintf("%s [%s]", "error: create exec in container", err.Error()), Fatal)
+		return nil
 	}
+
 	resp, err := cli.ContainerExecAttach(ctx, exec.ID, types.ExecConfig{
 		AttachStderr: true,
 		AttachStdout: true,
 		Cmd:           strings.Split(command, " "),
 	})
 	if err != nil {
-		return nil, err
+		verboseLog(fmt.Sprintf("%s [%s]", "error: attach exec to container", err.Error()), Fatal)
+		return nil
 	}
 	data, err := ioutil.ReadAll(resp.Reader)
 	if err != nil {
-		return nil, err
+		verboseLog(fmt.Sprintf("%s [%s]", "error: parse the response", err.Error()), Fatal)
+		return nil
 	}
 
-	return data, nil
+	return data
 }
 
-func getContainerIDs(ctx context.Context, cli *client.Client, containers []string) ([]string, error) {
+type containerDef struct {
+	id string
+	name string
+}
+
+func getContainerIDs(ctx context.Context, cli *client.Client, containers []string) ([]containerDef, error) {
 	images, err := cli.ContainerList(ctx, types.ContainerListOptions{})
 	if err != nil {
 		return nil, err
 	}
 
-	var ids []string
+	var ids []containerDef
 	for _, img := range images {
 		for _, name := range img.Names {
 			for _, container := range containers {
 				if container == removePrefix(name) {
-					ids = append(ids, img.ID)
+					ids = append(ids, containerDef{
+						id:   img.ID,
+						name: name,
+					})
 				}
 			}
 		}
@@ -117,4 +149,25 @@ func getContainerIDs(ctx context.Context, cli *client.Client, containers []strin
 
 func removePrefix(name string) string {
 	return strings.Replace(name, "/", "", -1)
+}
+
+const (
+	Info = iota
+	Fatal
+)
+
+func verboseLog(str string, level int) {
+	if verbose, ok := os.LookupEnv(EnvVerbose); ok && verbose != "true" {
+		return
+	} else if !ok {
+		return
+	}
+
+	switch level {
+	case Info:
+		log.Printf("[%s] %s", "INFO", str)
+	case Fatal:
+		log.Printf("[%s] %s", "FATAL", str)
+		os.Exit(1)
+	}
 }
